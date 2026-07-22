016abfe0d4ba1ac9dc693d405347b671f0633971
leslyhernadez1308-dev1307 <leslyhernadez1308@gmail.com>
2026-07-22 16:22:51 -0600

    Actualizar sitio

--- curiosidades-gatos/src/App.jsx
+++ curiosidades-gatos/src/App.jsx
@@ -6,11 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-[#f8f5ff] text-[#3b1c60] fuente-nunito relative">
-      <div className="bg-purple-600 text-white text-center py-2 px-4 font-bold text-sm shadow-md">
-        <span className="block sm:hidden">📱 Vista Móvil: ¡Esta página es 100% Responsiva!</span>
-        <span className="hidden sm:block md:hidden">📱 Vista Tablet: ¡Esta página es 100% Responsiva!</span>
-        <span className="hidden md:block">💻 Vista Escritorio: ¡Esta página es 100% Responsiva!</span>
-      </div>
+    
 
       <Navbar />
       <ContenidoPrincipal />
